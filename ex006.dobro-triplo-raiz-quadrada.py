6da8945dc8e32632b0bc476d125af8b6d00441b9
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2026-06-02 20:05:13 -0300

    add

---
+++ ex006.dobro-triplo-raiz-quadrada.py
@@ -0,0 +1,9 @@
+n = int(input("digite um número: "))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+i = n // 2
+print("O dobro de {} vale {}. ".format(n, d))
+print("O triplo de {} vale {}. ".format(n, t))
+print("A raiz quadrada de {} vale {}. ".format(n, r))
+print("O divisor inteiro de {} vale {}. ".format(n, i))
---
+++ ex007.media-aritmetica.py
@@ -0,0 +1,4 @@
+n1 = float(input("digite a primeira nota: "))
+n2 = float(input("digite a segunda nota: "))
+média = (n1 + n2) / 2
+print("A média entre {} e {} é igual a {}".format(n1, n2, média))
